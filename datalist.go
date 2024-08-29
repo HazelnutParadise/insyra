6000b1a0d06d4e047c29821aa03e6e1498330e47
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-08-29 23:01:03 +0800

    Update datalist.go

--- datalist.go
+++ datalist.go
@@ -11,7 +11,7 @@ type DataList struct {
 type IDataList interface {
 	Append(value interface{})
 	Get(index int) interface{}
-	Pop() (interface{}, error)
+	Pop() interface{}
 	Len() int
 	Sort(acending ...bool) error
 	Reverse()
@@ -43,6 +43,48 @@ func NewDataList(values ...interface{}) *DataList {
 	}
 }
 
+// Append adds a new value to the DataList.
+// The value can be of any type.
+// The value is appended to the end of the DataList.
 func (dl *DataList) Append(value interface{}) {
 	dl.data = append(dl.data, value)
+}
+
+// Get retrieves the value at the specified index in the DataList.
+// Supports negative indexing.
+// Returns nil if the index is out of bounds.
+// Returns the value at the specified index.
+func (dl *DataList) Get(index int) interface{} {
+	if index < -len(dl.data) || index >= len(dl.data) {
+		return nil
+	}
+	return dl.data[index]
+}
+
+// Pop removes and returns the last element from the DataList.
+// Returns the last element.
+// Returns nil if the DataList is empty.
+func (dl *DataList) Pop() interface{} {
+	n, err := sliceutil.Drt_PopFrom(&dl.data)
+	if err != nil {
+		return nil
+	}
+	return n
+}
+
+func (dl *DataList) Len() int {
+	return len(dl.data)
+}
+
+func (dl *DataList) Sort(acending ...bool) error {
+	// TODO
+	// 支援時間排序
+	// 支援字母排序
+	// 支援數字排序(sliceutil.Sort)
+	// 支援混合排序(Aa-Zz, 0-9, 時間)
+	return nil
+}
+
+func (dl *DataList) Reverse() {
+	sliceutil.Reverse(dl.data)
 }
