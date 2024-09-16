8db0c7db3922b5c244a81d9d56aeeda5d25e97b9
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-09-16 16:51:52 +0800

    Update init.go

--- init.go
+++ init.go
@@ -2,6 +2,6 @@ package insyra
 
 func init() {
 	SetDefaultConfig()
-	LogInfo("Insyra version 0.0.9")
+	LogInfo("Insyra version 0.0.10")
 	LogInfo("Thank you for using Insyra!\nSee our repository and documentation at https://github.com/HazelnutParadise/insyra .\nStar it if you like it.\n\n")
 }
