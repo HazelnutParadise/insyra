39d811fa295cdb2562ff4f7c894d11ff8c49f374
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-09-30 22:54:45 +0800

    Update datatable_test.go

--- datatable_test.go
+++ datatable_test.go
@@ -8,19 +8,22 @@ func IDataTableTest(dt IDataTable) {
 	return
 }
 
-func TestNewDataTable(t *testing.T) {
-	dt := NewDataTable()
-	if dt == nil {
-		t.Errorf("NewDataTable() returned nil")
-	}
-
-	IDataTableTest(dt)
+func TestIDataTable(t *testing.T) {
 	defer func() {
 		r := recover()
 		if r != nil {
 			t.Errorf("IDataTableTest() panicked with: %v", r)
 		}
 	}()
+	dt := NewDataTable()
+	IDataTableTest(dt)
+}
+
+func TestNewDataTable(t *testing.T) {
+	dt := NewDataTable()
+	if dt == nil {
+		t.Errorf("NewDataTable() returned nil")
+	}
 }
 
 func TestDataTable_AppendColumns(t *testing.T) {
