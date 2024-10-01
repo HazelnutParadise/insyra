b8674f1e375900e11175820900090e083164eb72
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-10-01 20:00:05 +0800

    Update datatable_test.go

--- datatable_test.go
+++ datatable_test.go
@@ -2,6 +2,7 @@ package insyra
 
 import (
 	"testing"
+	"time"
 )
 
 func IDataTableTest(dt IDataTable) {
@@ -41,5 +42,32 @@ func TestDataTable_AppendRowsFromDataList(t *testing.T) {
 	dt.AppendRowsFromDataList(dl)
 	if r, c := dt.Size(); r != 1 || c != 3 {
 		t.Errorf("AppendRowsFromDataList() did not add the row correctly")
+	}
+}
+
+func TestDataTable_AppendRowsByIndex(t *testing.T) {
+	dt := NewDataTable()
+	dt.AppendRowsByIndex(map[string]interface{}{"A": 1, "B": 2, "C": 3})
+	dt.Show()
+	if r, c := dt.Size(); r != 1 || c != 3 {
+		t.Errorf("AppendRowsByIndex() did not add the row correctly")
+	}
+}
+
+func TestDataTable_AppendRowsByName(t *testing.T) {
+	dt := NewDataTable()
+	dt.AppendRowsByName(map[string]interface{}{"first": 1, "second": 2, "third": 3})
+	dt.Show()
+	if r, c := dt.Size(); r != 1 || c != 3 {
+		t.Errorf("AppendRowsByName() did not add the row correctly")
+	}
+}
+
+func TestDataTable_GetLastModifiedTimestamp(t *testing.T) {
+	dt := NewDataTable()
+	dl := NewDataList(1, 2, 3)
+	dt.AppendRowsFromDataList(dl)
+	if dt.GetLastModifiedTimestamp() != time.Now().Unix() {
+		t.Errorf("GetLastModifiedTimestamp() did not return the correct timestamp")
 	}
 }
