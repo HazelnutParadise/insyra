f868d0149c57c31d967f786aaec39bb26870cc20
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-10-16 22:09:01 +0800

    Update lingo_processor.go

--- lpgen/lingo_processor.go
+++ lpgen/lingo_processor.go
@@ -3,9 +3,11 @@ package lpgen
 // 索引字母 I, J, K, L, M, N
 
 type lingoProcessResult struct {
-	Tokens *[]lingoToken
+	Tokens []lingoToken
 }
 
-func lingoProcessor(result *lingoExtractResult) *lingoProcessResult {
-	return &lingoProcessResult{}
+func lingoProcessor(extractResult *lingoExtractResult) *lingoProcessResult {
+	result := &lingoProcessResult{}
+	result.Tokens = extractResult.Tokens
+	return result
 }
