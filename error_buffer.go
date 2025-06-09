0b7c1b4592cf2fc763c5d4c747f890f9cc3eeaa1
TimLai666 <tim930102@icloud.com>
2025-06-09 15:28:30 +0800

    新增 PopErrorByPackageName 和 PopErrorByFuncName 函數以支持按包名和函數名彈出錯誤，並更新錯誤切片的處理邏輯

--- error_buffer.go
+++ error_buffer.go
@@ -1,6 +1,9 @@
 package insyra
 
-import "sync"
+import (
+	"slices"
+	"sync"
+)
 
 type ErrPoppingMode int
 
@@ -68,6 +71,82 @@ func PopError(mode ErrPoppingMode) (LogLevel, string) {
 		errorSlice = errorSlice[:len(errorSlice)-1]
 	}
 	return err.errType, err.message
+}
+
+func PopErrorByPackageName(packageName string, mode ErrPoppingMode) (LogLevel, string) {
+	errorMutex.Lock()
+	defer errorMutex.Unlock()
+
+	if len(errorSlice) == 0 {
+		return LogLevelInfo, ""
+	}
+
+	idxToPop := -1
+
+	switch mode {
+	case ErrPoppingModeFIFO:
+		// Find the first occurrence
+		for i, e := range errorSlice {
+			if e.packageName == packageName {
+				idxToPop = i
+				break
+			}
+		}
+	case ErrPoppingModeLIFO:
+		// Find the last occurrence
+		for i := len(errorSlice) - 1; i >= 0; i-- {
+			if errorSlice[i].packageName == packageName {
+				idxToPop = i
+				break
+			}
+		}
+	}
+
+	if idxToPop != -1 {
+		err := errorSlice[idxToPop]
+		errorSlice = slices.Delete(errorSlice, idxToPop, idxToPop+1)
+		return err.errType, err.message
+	}
+
+	return LogLevelInfo, "" // No error found for the given package name
+}
+
+func PopErrorByFuncName(packageName, funcName string, mode ErrPoppingMode) (LogLevel, string) {
+	errorMutex.Lock()
+	defer errorMutex.Unlock()
+
+	if len(errorSlice) == 0 {
+		return LogLevelInfo, ""
+	}
+
+	idxToPop := -1
+
+	switch mode {
+	case ErrPoppingModeFIFO:
+		// Find the first occurrence
+		for i, e := range errorSlice {
+			if e.packageName == packageName && e.fnName == funcName {
+				idxToPop = i
+				break
+			}
+		}
+	case ErrPoppingModeLIFO:
+		// Find the last occurrence
+		for i := len(errorSlice) - 1; i >= 0; i-- {
+			if errorSlice[i].packageName == packageName && errorSlice[i].fnName == funcName {
+				idxToPop = i
+				break
+			}
+		}
+	}
+
+	if idxToPop != -1 {
+		err := errorSlice[idxToPop]
+		errorSlice = slices.Delete(errorSlice, idxToPop, idxToPop+1)
+		return err.errType, err.message
+	}
+
+	return LogLevelInfo, "" // No error found for the given package and function name
 }
 
 func PopErrorAndCallback(mode ErrPoppingMode, callback func(errType LogLevel, packageName string, funcName string, errMsg string)) {
