54f64e96c255b5a2cddd49e4111b5154fb3fa57c
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2025-09-13 11:01:01 +0800

    feat: 新增 RFMConfig 結構及 RFM 函數，為 RFM 功能開發做準備

--- mkt/rfm.go
+++ mkt/rfm.go
@@ -1,3 +1,12 @@
 package mkt
 
+import "github.com/HazelnutParadise/insyra"
+
+type RFMConfig struct {
+	CustomerIDCol string // The column index(A, B, C, ...) of customer ID in the data table
+	TradingDayCol string // The column index(A, B, C, ...) of trading day in the data table
+	AmountCol     string // The column index(A, B, C, ...) of amount in the data table
+}
+
 // todo
+func RFM(dt insyra.IDataTable, rfmConfig RFMConfig) {}
