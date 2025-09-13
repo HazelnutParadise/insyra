6e988a0169b19c70c5347660d2c3e7d71107b50c
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2025-09-13 14:39:38 +0800

    feat: 更新 RFMConfig 結構，新增 NumGroups 欄位以支持客戶分組

--- mkt/rfm.go
+++ mkt/rfm.go
@@ -6,6 +6,7 @@ type RFMConfig struct {
 	CustomerIDCol string // The column index(A, B, C, ...) of customer ID in the data table
 	TradingDayCol string // The column index(A, B, C, ...) of trading day in the data table
 	AmountCol     string // The column index(A, B, C, ...) of amount in the data table
+	NumGroups     uint   // The number of groups to divide the customers into
 }
 
 // todo
