e0492b1e1afcba5f6aeb99d45d994a9e47620c75
TimLai666 <tim930102@icloud.com>
2025-09-20 16:47:06 +0800

    feat: 更新初始化日誌，將「使用」改為「採用」以提升用語一致性

--- init.go
+++ init.go
@@ -3,5 +3,5 @@ package insyra
 func init() {
 	SetDefaultConfig()
 	initCCLFunctions()
-	LogInfo("", "", "Thank you for using Insyra %s(v%s)!\nOfficial website: https://insyra.hazelnut-paradise.com\n\n", VersionName, Version)
+	LogInfo("", "", "Thank you for adopting Insyra %s(v%s)!\nOfficial website: https://insyra.hazelnut-paradise.com\n\n", VersionName, Version)
 }
