4e3abbdc3ff0a06356cc50956e8a3dd1194d1012
TimLai666 <tim930102@icloud.com>
2025-10-04 19:04:15 +0800

    Remove deprecated GPA rotation functions and associated wrapper for factor analysis
    - Deleted functions for various GPA rotation methods: vgQPst, vgQSimplimax, vgQTandemI, vgQTandemII, vgQTarget, vgQVarimin.
    - Removed the psych_fa_wrapper and psych_fac_full implementations, which included the main factor analysis function and its statistics.
    - Cleaned up the psych_factor_stats and psych_glb_algebraic files, removing unused functions and definitions.
    - These changes streamline the codebase by eliminating outdated methods and focusing on more efficient implementations.

--- stats/internal/fa/GPArotation_GPForth.go
+++ stats/internal/fa/GPArotation_GPForth.go
@@ -52,55 +52,6 @@ func GPForth(A *mat.Dense, Tmat *mat.Dense, normalize bool, eps float64, maxit i
 			"f":      f,
 			"Method": "vgQ." + method,
 		}
-	case "varimin":
-		Gq, f, _ := vgQVarimin(L)
-		VgQ = map[string]interface{}{
-			"Gq":     Gq,
-			"f":      f,
-			"Method": "vgQ." + method,
-		}
-	case "quartimin":
-		Gq, f, _ := vgQQuartimin(L)
-		VgQ = map[string]interface{}{
-			"Gq":     Gq,
-			"f":      f,
-			"Method": "vgQ." + method,
-		}
-	case "bentler":
-		Gq, f, _ := vgQBentler(L)
-		VgQ = map[string]interface{}{
-			"Gq":     Gq,
-			"f":      f,
-			"Method": "vgQ." + method,
-		}
-	case "oblimax":
-		Gq, f, _ := vgQOblimax(L)
-		VgQ = map[string]interface{}{
-			"Gq":     Gq,
-			"f":      f,
-			"Method": "vgQ." + method,
-		}
-	case "entropy":
-		Gq, f, _ := vgQEntropy(L)
-		VgQ = map[string]interface{}{
-			"Gq":     Gq,
-			"f":      f,
-			"Method": "vgQ." + method,
-		}
-	case "tandemI":
-		Gq, f, _ := vgQTandemI(L)
-		VgQ = map[string]interface{}{
-			"Gq":     Gq,
-			"f":      f,
-			"Method": "vgQ." + method,
-		}
-	case "tandemII":
-		Gq, f, _ := vgQTandemII(L)
-		VgQ = map[string]interface{}{
-			"Gq":     Gq,
-			"f":      f,
-			"Method": "vgQ." + method,
-		}
 	default:
 		Gq, f, _ := vgQVarimax(L)
 		VgQ = map[string]interface{}{
@@ -128,48 +79,6 @@ func GPForth(A *mat.Dense, Tmat *mat.Dense, normalize bool, eps float64, maxit i
 		}
 	case "quartimax":
 		Gq2, f2, _ := vgQQuartimax(L)
-		VgQt = map[string]interface{}{
-			"Gq": Gq2,
-			"f":  f2,
-		}
-	case "varimin":
-		Gq2, f2, _ := vgQVarimin(L)
-		VgQt = map[string]interface{}{
-			"Gq": Gq2,
-			"f":  f2,
-		}
-	case "quartimin":
-		Gq2, f2, _ := vgQQuartimin(L)
-		VgQt = map[string]interface{}{
-			"Gq": Gq2,
-			"f":  f2,
-		}
-	case "bentler":
-		Gq2, f2, _ := vgQBentler(L)
-		VgQt = map[string]interface{}{
-			"Gq": Gq2,
-			"f":  f2,
-		}
-	case "oblimax":
-		Gq2, f2, _ := vgQOblimax(L)
-		VgQt = map[string]interface{}{
-			"Gq": Gq2,
-			"f":  f2,
-		}
-	case "entropy":
-		Gq2, f2, _ := vgQEntropy(L)
-		VgQt = map[string]interface{}{
-			"Gq": Gq2,
-			"f":  f2,
-		}
-	case "tandemI":
-		Gq2, f2, _ := vgQTandemI(L)
-		VgQt = map[string]interface{}{
-			"Gq": Gq2,
-			"f":  f2,
-		}
-	case "tandemII":
-		Gq2, f2, _ := vgQTandemII(L)
 		VgQt = map[string]interface{}{
 			"Gq": Gq2,
 			"f":  f2,
@@ -261,48 +170,6 @@ func GPForth(A *mat.Dense, Tmat *mat.Dense, normalize bool, eps float64, maxit i
 				}
 			case "quartimax":
 				GqNew, fNew, _ := vgQQuartimax(L)
-				VgQt = map[string]interface{}{
-					"Gq": GqNew,
-					"f":  fNew,
-				}
-			case "varimin":
-				GqNew, fNew, _ := vgQVarimin(L)
-				VgQt = map[string]interface{}{
-					"Gq": GqNew,
-					"f":  fNew,
-				}
-			case "quartimin":
-				GqNew, fNew, _ := vgQQuartimin(L)
-				VgQt = map[string]interface{}{
-					"Gq": GqNew,
-					"f":  fNew,
-				}
-			case "bentler":
-				GqNew, fNew, _ := vgQBentler(L)
-				VgQt = map[string]interface{}{
-					"Gq": GqNew,
-					"f":  fNew,
-				}
-			case "oblimax":
-				GqNew, fNew, _ := vgQOblimax(L)
-				VgQt = map[string]interface{}{
-					"Gq": GqNew,
-					"f":  fNew,
-				}
-			case "entropy":
-				GqNew, fNew, _ := vgQEntropy(L)
-				VgQt = map[string]interface{}{
-					"Gq": GqNew,
-					"f":  fNew,
-				}
-			case "tandemI":
-				GqNew, fNew, _ := vgQTandemI(L)
-				VgQt = map[string]interface{}{
-					"Gq": GqNew,
-					"f":  fNew,
-				}
-			case "tandemII":
-				GqNew, fNew, _ := vgQTandemII(L)
 				VgQt = map[string]interface{}{
 					"Gq": GqNew,
 					"f":  fNew,
